1cba423be22e99c2ccce9577533fb600b7b53f74
ardiansetya <ardiansetya129@gmail.com>
2025-03-20 14:59:16 +0700

    create model user dan periksa

---
+++ database/seeders/PeriksaSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Periksas;
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PeriksaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Periksas::create([
+            'id_pasien' => 2,
+            'id_dokter' => 1,
+            'tgl_periksa'=> Carbon::now(),
+            'catatan'=> 'pasien mengalami demam tinggi dan batuk',
+            'biaya_periksa' => 150000
+        ]);
+    }
+}
